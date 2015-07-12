193443115c28e3046eef5fa24c2c8d34a3f33b0b
ShinDarth <borzifrancesco@gmail.com>
2015-07-12 09:51:48 +0200

    Fit with latest TrinityCore

--- functions.php
+++ functions.php
@@ -9,10 +9,10 @@ function printTableBody($limit)
   if (!is_int($limit))
     return;
 
-  $query = sprintf("SELECT t1.id, t2.title, COUNT(t1.quest_abandon_time) AS abandoned_times, COUNT(t1.quest_complete_time) AS completed_times, MAX(t1.quest_abandon_time) AS last_abandoned, MAX(t1.quest_complete_time) AS last_completed " .
+  $query = sprintf("SELECT t1.id, t2.LogTitle, COUNT(t1.quest_abandon_time) AS abandoned_times, COUNT(t1.quest_complete_time) AS completed_times, MAX(t1.quest_abandon_time) AS last_abandoned, MAX(t1.quest_complete_time) AS last_completed " .
                    "FROM (SELECT id, quest_abandon_time, quest_complete_time, core_hash, core_revision FROM %s.quest_tracker) AS t1 " .
-                   "JOIN (SELECT id, title FROM %s.quest_template) AS t2 " .
-                   "ON t1.id = t2.id " .
+                   "JOIN (SELECT ID, LogTitle FROM %s.quest_template) AS t2 " .
+                   "ON t1.id = t2.ID " .
                    "GROUP BY t1.id " .
                    "HAVING abandoned_times > 0 " .
                    "ORDER BY abandoned_times DESC " .
@@ -32,7 +32,7 @@ function printTableBody($limit)
            $row['id'],
            $url,
            $row['id'],
-           $row['title'],
+           $row['LogTitle'],
            $row['abandoned_times'],
            $row['completed_times'],
            $row['last_abandoned'],
